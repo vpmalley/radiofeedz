15477e2e956d2287736b437e45004f167e55817f
Vincent Malley <vincent.malley@insalien.org>
2016-02-16 23:59:07 +0100

    trying to add stats

---
+++ app/src/main/java/fr/vpm/audiorss/process/StatProperties.java
@@ -0,0 +1,46 @@
+package fr.vpm.audiorss.process;
+
+import android.util.Log;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * Created by vince on 16/02/16.
+ */
+public class StatProperties {
+
+  private static final String STAT_PATH = "stats";
+  private static StatProperties instance;
+
+  private Properties properties;
+
+  public StatProperties getInstance() {
+    if (instance == null) {
+      instance = new StatProperties();
+    }
+    return instance;
+  }
+
+  public StatProperties() {
+    properties = new Properties();
+  }
+
+  public void load() {
+    try {
+      properties.load(new FileInputStream(STAT_PATH));
+    } catch (IOException e) {
+      Log.e("stats", "overriding stats");
+    }
+  }
+
+  public void save() {
+    try {
+      properties.store(new FileOutputStream(STAT_PATH), "stored updated props");
+    } catch (IOException e) {
+      Log.e("stats", "failed storing stats");
+    }
+  }
+}
